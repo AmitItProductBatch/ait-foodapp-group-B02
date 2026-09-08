a78a1ea13a652cf0a3e35e61a7ced5be8f975188
ashishbawne28 <ashishbawane28@gmail.com>
2026-09-08 20:44:02 +0530

    OFD - 103_User Login
    OFD - 103_User Login

--- food-delivery-backend/src/main/java/com/ait/app/service/UserService.java
+++ food-delivery-backend/src/main/java/com/ait/app/service/UserService.java
@@ -1,8 +1,13 @@
 package com.ait.app.service;
 
+import com.ait.app.dto.UserResponse;
 import com.ait.app.entity.User;
 
 public interface UserService {
 
 	public void saveUser(User user);
+
+	UserResponse getUser(int id);
+
+	void deleteUser(int id);
 }
