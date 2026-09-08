6a27128613cda74b4466a3591ed93f70d6218271
Abhishek Tatale <tataleabhishek@gmail.com>
2026-09-08 18:08:47 +0530

    OFD-102: I have implemented user address CRUD operations (UserAddress)

--- food-delivery-backend/src/main/java/com/ait/app/entity/UserAddress.java
+++ food-delivery-backend/src/main/java/com/ait/app/entity/UserAddress.java
@@ -13,31 +13,19 @@ public class UserAddress {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
-
-    // --- Specific Address Breakdown ---
     
-  
-    private String houseNo; // e.g., "Flat 402", "House 12-A"
+    private String houseNo; 
+    private String buildingName;
 
-    private String buildingName; // e.g., "Greenwood Apartments"
-
-
-    private String street; // e.g., "1st Main Road, Sector 4"
-
-    private String landmark; // e.g., "Behind City Hospital"
-
-    private String area; // e.g., "Koramangala"
-    
+    private String street; 
+    private String landmark;
+    private String area; 
     private String city;
 
     private String state;
 
     private Long pincode;
 
-    // --- Food Delivery Specific Meta-data ---
-
-    private String addressType; // HOME, WORK, OTHER
-
+    private String addressType; 
     //private User user;
 }
