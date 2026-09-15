a4bb5b8440a09068be7e382c8aeb37abdb9ee492
ashishbawne28 <ashishbawane28@gmail.com>
2026-09-15 20:41:06 +0530

    OFD-146_User0Requirment
    OFD-146_User0Requirment

--- food-delivery-backend/src/main/java/com/ait/app/repository/UserRepository.java
+++ food-delivery-backend/src/main/java/com/ait/app/repository/UserRepository.java
@@ -1,11 +1,21 @@
 package com.ait.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ait.app.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 	boolean existsByEmail(String email);
-	boolean existsByMobile(long mobile);
+
+	boolean existsByMobile(String mobile);
+
+	@Query("SELECT COUNT(u) > 0 FROM User u WHERE u.email = :email AND u.id != :id")
+	boolean existsEmailForOtherUser(@Param("email") String email, @Param("id") int id);
+
+	
+	@Query("SELECT COUNT(u) > 0 FROM User u WHERE u.mobile = :mobile AND u.id != :id")
+	boolean existsMobileForOtherUser(@Param("mobile") String mobile, @Param("id") int id);
 }
